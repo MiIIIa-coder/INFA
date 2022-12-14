6d6abe83f6d171619db6dd4eb571bb91ff80d00f
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-12-15 01:28:43 +0300

    Update problem_CM.c
    added data with different sizes elements

--- problem_CM.c
+++ problem_CM.c
@@ -2,22 +2,48 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define structura 0
+
+#if structura
 struct triple {
     int x, y, z;
 };
+#endif
 
 /*
+void swap_sizes(int *sizes, int first, int second)
+{
+    int swap_elt;
+
+    swap_elt = sizes[first];
+    sizes[first] = sizes[second];
+    sizes[second] = swap_elt;
+
+}
+*/
+
+
 int cmp(void *lhs, int lsz, void *rhs, int rsz)
 {
+    int i = 0;
     const int *lhsi = (const int *) lhs;
     const int *rhsi = (const int *) rhs;
 
+/*
     if (*lhsi < *rhsi)
         return 1;
     else
         return 0;
-}*/
+*/
 
+    for (i = 0; i < 4; ++i)
+        if (*((char *) lhsi + i) < *((char *) rhsi + i))
+            return 1;
+
+    return 0;
+}
+
+#if structura
 int cmp(void *lhs, int lsz, void *rhs, int rsz)
 {
     struct triple const *lstr = (struct triple const *) lhs;
@@ -29,6 +55,9 @@ int cmp(void *lhs, int lsz, void *rhs, int rsz)
         return (lstr->y < rstr->y);
     return (lstr->x < rstr->x);
 }
+
+*
+#endif
 
 typedef int (*xcmp_t)(void *lhs, int lsz, void *rhs, int rsz);
 
@@ -45,12 +74,12 @@ int count_number_byte(int *sizes, int start_byte, int end_byte, int byte)
 void my_merge(void *mem, int *sizes, int l, int m, int r, xcmp_t cmp)
 {
 
-    int i;
-    int ptr_sizes;
-    int j;
+    int i = 0;
+    int ptr_sizes = 0;
+    int j = 0;
     int memory = 0;
-    int place;
-    int index;
+    int place = 0;
+    int index = 0;
 
     int first_byte = 0;         //<=> i
     int first_byte_saved;
@@ -71,10 +100,11 @@ void my_merge(void *mem, int *sizes, int l, int m, int r, xcmp_t cmp)
 
     first_byte = count_number_byte(sizes, 0, l, 0);
     first_byte_saved = first_byte;
-    
+
     middle_byte = count_number_byte(sizes, l, m + 1, first_byte);
 
     for (; i <= m; i++) {
+        //while (mem[j] <= mem[i] && j <= r)
         while (cmp((mem + middle_byte), sizes[j], (mem + first_byte), sizes[i]) && j <= r) {    //here comparer
             for (place = 0; place < sizes[j]; byte_ptr++, place++)
                 *(mem_new + byte_ptr) =
@@ -84,7 +114,8 @@ void my_merge(void *mem, int *sizes, int l, int m, int r, xcmp_t cmp)
             ++ptr_sizes;
             ++j;
         }
-        
+
+        //arr_new[k++] = arr[i];
         for (place = 0; place < sizes[i]; byte_ptr++, place++)
             *(mem_new + byte_ptr) = *((char *) mem + first_byte + place);
         first_byte += sizes[i];
@@ -93,6 +124,7 @@ void my_merge(void *mem, int *sizes, int l, int m, int r, xcmp_t cmp)
     }
 
     while (j <= r) {
+        //arr_new[k++] = arr[j++];
         for (place = 0; place < sizes[j]; byte_ptr++, place++)
             *(mem_new + byte_ptr) = *((char *) mem + middle_byte + place);
         middle_byte += sizes[j];
@@ -134,11 +166,12 @@ void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp)
 int main()
 {
     int i = 0;
-    //int nelts = 10;
-    int res = 0;
-    //int mem[] = { 8, 9, 9, 7, 1, 7, 9, 9, 1, 8 };
-    //int sizes[] = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
-#if 1
+    int nelts = 5;
+    //int res = 0;
+    int mem[] = { 8, 1, 5, 6, 4, 7, 9, 9, 9, 1, 9, 9, 9 };
+    int sizes[] = { 8, 12, 4, 12, 16 };
+
+#if structura
     struct triple *mem;
     int *sizes;
     int nelts;
@@ -154,7 +187,6 @@ int main()
         assert(res == 3);
         sizes[i] = sizeof(mem[i]);
     }
-#endif
 
     xmsort((void *) mem, sizes, nelts, cmp);
 
@@ -164,6 +196,20 @@ int main()
         printf("%d %d %d", mem[i].x, mem[i].y, mem[i].z);
         printf("\n");
     }
+#endif
+
+#if (1 - structura)
+    xmsort((void *) mem, sizes, nelts, cmp);
+
+    for (i = 0; i < 13; ++i)
+        printf("%d ", mem[i]);
+
+    printf("\n");
+    for (i = 0; i < 5; ++i)
+        printf("%d ", sizes[i]);
+    printf("\n");
 
     return 0;
+#endif
+
 }
